5c3c848cddc0a9058ac024ba0729fb4aa2378aa1
Adrian Cerbaro <178304@upf.br>
2025-05-14 15:25:57 -0300

    fix(frontend): remove /plugins from .gitignore

---
+++ apps/frontend/plugins/api.ts
@@ -0,0 +1,40 @@
+export default defineNuxtPlugin(async (nuxtApp) => {
+  let csrfToken: string | null = null;
+
+  const api: ReturnType<typeof $fetch.create> = $fetch.create({
+    baseURL: "/api",
+    retryStatusCodes: [408, 409, 425, 429, 500, 502, 503, 504, 403],
+    async onRequest({ options }) {
+      if (csrfToken) {
+        options.headers.set("x-csrf-token", csrfToken);
+      }
+    },
+    async onResponseError(ctx) {
+      if (ctx.response.status === 401) {
+        await nuxtApp.runWithContext(() => navigateTo("/login"));
+      } else if (
+        ctx.response.status === 403 &&
+        ctx.response._data?.code === "CSRF_ERROR"
+      ) {
+        await updateCsrfToken();
+      }
+    },
+  });
+
+  // Fetch CSRF token
+  const updateCsrfToken = async () => {
+    csrfToken = await api<{ csrfToken: string }>("/csrf", {
+      credentials: "include",
+    }).then((res) => res.csrfToken);
+  };
+
+  // Fetch CSRF token on initial load
+  await updateCsrfToken();
+
+  // Expose to useNuxtApp().$api
+  return {
+    provide: {
+      api,
+    },
+  };
+});
